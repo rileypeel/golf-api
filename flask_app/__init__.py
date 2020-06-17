3594ecf5fa6b1dbb983d228e5d80f625d882f44e
Riley Peel <rpeel4@outlook.com>
2020-06-17 12:22:38 -0700

    fixed bug in cors after request method

--- flask_app/__init__.py
+++ flask_app/__init__.py
@@ -21,6 +21,7 @@ def create_app(test_config=None):
     def after_request(response):
         response.headers.add('Access-Control-Allow-Headers', 'Content-Type, Authorizatino')
         response.headers.add('Access-Control-Allow-Methods', 'GET, POST, PATCH, DELETE, OPTIONS')
+        return response
 
     return app
 
